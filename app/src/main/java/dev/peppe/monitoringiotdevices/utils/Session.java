9660470878fcd6fac1e915497ce27bdfe7a832c3
peppevaccaro <peppevaccaro10@gmail.com>
2018-09-28 11:35:29 +0200

    Upload New Project into Git

--- app/src/main/java/dev/peppe/monitoringiotdevices/utils/Session.java
+++ app/src/main/java/dev/peppe/monitoringiotdevices/utils/Session.java
@@ -1,4 +1,47 @@
 package dev.peppe.monitoringiotdevices.utils;
 
 public class Session {
+    public String serverUri;
+    public String clientId;
+    public String username;
+    public String password;
+
+    public Session (String uri,String client,String user, String passw){
+        this.serverUri = uri;
+        this.clientId = client;
+        this.username = user;
+        this.password = passw;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getServerUri() {
+        return serverUri;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setServerUri(String serverUri) {
+        this.serverUri = serverUri;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
 }
--- app/src/main/java/dev/peppe/monitoringiotdevices/utils/Subscription.java
+++ app/src/main/java/dev/peppe/monitoringiotdevices/utils/Subscription.java
@@ -1,5 +1,28 @@
 package dev.peppe.monitoringiotdevices.utils;
 
 public class Subscription {
-    
+    public String topic;
+    public int qos;
+
+    public Subscription(String topic,int qos){
+        this.topic = topic;
+        this.qos = qos;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getQos() {
+        return qos;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setQos(int qos) {
+        this.qos = qos;
+    }
 }
+
